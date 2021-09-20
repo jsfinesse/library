094e4012128718bd92e9a6994e39ef459e9a8d2a
Prithu Singh <prithusingh0327@gmail.com>
2021-09-20 18:05:26 +0530

    Added js to add new books and display them

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let myLibrary = [];
 
-function book() 
+function book(title, author, pages, read) 
 {
     this.title = title;
     this.author = author;
@@ -11,3 +11,60 @@ function book()
     }
 }
 
+const defaultBook = new book('The Theory of Everything', 'Stephen Hawking', '64', true);
+myLibrary.push(defaultBook);
+
+function addBookToLibrary()
+{
+    const title = document.getElementById('title').value;
+    const author = document.getElementById('author').value;
+    const pages = document.getElementById('pages').value;
+    const isRead = document.getElementById('read').checked;
+    // console.log(title, author, pages, isRead);
+
+    if(myLibrary.some(item => item.title === title)) {
+        alert("This book already exists in your library");
+        return;
+    }
+
+    const newBook = new book(title, author, pages, isRead);
+    myLibrary.push(newBook);
+    // console.log(myLibrary);
+    displayLibrary(newBook);
+}
+
+const booksList = document.querySelector('.booksList');
+
+function displayLibrary(book)
+{
+    const itemBody = document.createElement('div');
+    const titlePara = document.createElement('p');
+    const authorPara = document.createElement('p');
+    const pagesPara = document.createElement('p');
+    const readPara = document.createElement('p');
+    const deleteBttn = document.createElement('button');
+
+    itemBody.classList.add('items');
+
+    titlePara.textContent = `Title: ${book.title}`;
+    authorPara.textContent = `Author: ${book.author}`;
+    pagesPara.textContent = `Pages: ${book.pages}`;
+    readPara.textContent = `Read: ${book.read ? 'Yes' : 'No'}`;
+    deleteBttn.textContent = `Delete`;
+
+    itemBody.appendChild(titlePara);
+    itemBody.appendChild(authorPara);
+    itemBody.appendChild(pagesPara);
+    itemBody.appendChild(readPara);
+    itemBody.appendChild(deleteBttn);
+    booksList.appendChild(itemBody);
+}
+
+function loopLibrary()
+{
+    myLibrary.forEach(function (book) {
+        displayLibrary(book);
+    });
+}
+
+loopLibrary();
