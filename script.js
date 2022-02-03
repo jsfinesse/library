03414dac8d8a2512f71864da4018cd538fa055f7
Prithu Singh <prithusingh0327@gmail.com>
2022-02-03 11:54:56 +0530

    Created modal for new entries

--- script.js
+++ script.js
@@ -1,28 +1,31 @@
 let myLibrary = [];
 
-function book(title, author, pages, read) 
-{
+function book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
-    this.info = function() {
-        return (`${title} by ${author}, ${pages}, Read status ${read}`);
-    }
+    this.info = function () {
+        return `${title} by ${author}, ${pages}, Read status ${read}`;
+    };
 }
 
-const defaultBook = new book('The Theory of Everything', 'Stephen Hawking', '64', true);
+const defaultBook = new book(
+    "The Theory of Everything",
+    "Stephen Hawking",
+    "64",
+    true
+);
 myLibrary.push(defaultBook);
 
-function addBookToLibrary()
-{
-    const title = document.getElementById('title').value;
-    const author = document.getElementById('author').value;
-    const pages = document.getElementById('pages').value;
-    const isRead = document.getElementById('read').checked;
+function addBookToLibrary() {
+    const title = document.getElementById("title").value;
+    const author = document.getElementById("author").value;
+    const pages = document.getElementById("pages").value;
+    const isRead = document.getElementById("read").checked;
     // console.log(title, author, pages, isRead);
 
-    if(myLibrary.some(item => item.title === title)) {
+    if (myLibrary.some((item) => item.title === title)) {
         alert("This book already exists in your library");
         return;
     }
@@ -33,31 +36,29 @@ function addBookToLibrary()
     displayLibrary(newBook);
 }
 
-function removeBook(e)
-{
+function removeBook(e) {
     const parentNode = e.parentElement;
     // console.log(parentNode);
     parentNode.remove();
 }
 
-const booksList = document.querySelector('.booksList');
+const booksList = document.querySelector(".booksList");
 
-function displayLibrary(book)
-{
-    const itemBody = document.createElement('div');
-    const titlePara = document.createElement('p');
-    const authorPara = document.createElement('p');
-    const pagesPara = document.createElement('p');
-    const readPara = document.createElement('p');
-    const deleteBttn = document.createElement('button');
+function displayLibrary(book) {
+    const itemBody = document.createElement("div");
+    const titlePara = document.createElement("p");
+    const authorPara = document.createElement("p");
+    const pagesPara = document.createElement("p");
+    const readPara = document.createElement("p");
+    const deleteBttn = document.createElement("button");
 
-    itemBody.classList.add('items');
-    deleteBttn.setAttribute('onclick', 'removeBook(this)');
+    itemBody.classList.add("items");
+    deleteBttn.setAttribute("onclick", "removeBook(this)");
 
     titlePara.textContent = `Title: ${book.title}`;
     authorPara.textContent = `Author: ${book.author}`;
     pagesPara.textContent = `Pages: ${book.pages}`;
-    readPara.textContent = `Read: ${book.read ? 'Yes' : 'No'}`;
+    readPara.textContent = `Read: ${book.read ? "Yes" : "No"}`;
     deleteBttn.textContent = `Delete`;
 
     itemBody.appendChild(titlePara);
@@ -68,8 +69,20 @@ function displayLibrary(book)
     booksList.appendChild(itemBody);
 }
 
-function loopLibrary()
-{
+const addBook = document.querySelector(".add-book");
+const modalBg = document.querySelector(".modal-bg");
+
+addBook.addEventListener("click", () => {
+    modalBg.classList.add("modal-active");
+});
+
+const modalClose = document.querySelector(".modal-close");
+
+modalClose.addEventListener("click", () => {
+    modalBg.classList.remove("modal-active");
+});
+
+function loopLibrary() {
     myLibrary.forEach(function (book) {
         displayLibrary(book);
     });
